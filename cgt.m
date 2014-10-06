d6189ea9a2bbc38631f1776fe27c032ac6673eb4
albertorevel <areveljarn@if501-213-07.insa-lyon.fr>
2014-10-06 15:37:29 +0200

    Creation graphes_cgt et modification cgt.m

--- cgt.m
+++ cgt.m
@@ -1,11 +1,14 @@
-function [x,fval] = cgt(T1,ConstraintsA,ConstraintsB)
+function [x,fval] = cgt()
 %CGT Summary of this function goes here
 %   Detailed explanation goes here
+
+matrix();
+
 Tps = sum(T1,2);
 T1l = [T1(:,3) T1(:,5)];
 Tpsl = sum(T1l,2);
 
-f = transpose(Tpsl ./ Tps);
+f = (Tpsl ./ Tps);
 
 [x,fval] = linprog(f,ConstraintsA,ConstraintsB,[],[],zeros(6,1));
 
---
+++ graphes_cgt.m
@@ -0,0 +1,41 @@
+function graphes_cgt( )
+%GRAPHES_CGT Summary of this function goes here
+%   Detailed explanation goes here
+    matrix();
+    [best_x_ben, f_ben] = benefice();
+    ben_max = f_ben * best_x_ben;
+
+    ConstraintsA(11, :) = -f_ben;
+
+    cgt_res_3 = zeros(101, 1);
+    cgt_res_5 = zeros(101, 1);
+    cgt_res_35 = zeros(101, 1);
+    for i = 0:100
+        pourcentage = i / 100;
+        
+        Tps = sum(T1,2);
+        T1l_3 = [T1(:,3)];
+        T1l_5 = [T1(:,5)];
+        T1l_35 = [T1(:,3) T1(:,5)];
+        Tpsl_3 = sum(T1l_3,2);
+        Tpsl_5 = sum(T1l_5,2);
+        Tpsl_35 = sum(T1l_35,2);
+
+        f_cgt_3 = (Tpsl_3 ./ Tps);
+        f_cgt_5 = (Tpsl_5 ./ Tps);
+        f_cgt_35 = (Tpsl_35 ./ Tps);
+
+        ConstraintsB(11) = -ben_max * pourcentage;
+        best_x_cgt_3 = linprog(f_cgt_3, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
+        best_x_cgt_5 = linprog(f_cgt_5, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
+        best_x_cgt_35 = linprog(f_cgt_35, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
+        % L'indexation commence à 1, donc de 1 à 101 au lieu de 0 à 100
+        
+        cgt_res_3(i + 1) = f_cgt_3' * best_x_cgt_3;
+        cgt_res_5(i + 1) = f_cgt_5' * best_x_cgt_5;
+        cgt_res_35(i + 1) = f_cgt_35' * best_x_cgt_35;
+    end
+    plot(0:0.01:1, cgt_res_3, 'g-', 0:0.01:1, cgt_res_5, 'r-', 0:0.01:1, cgt_res_35);
+end
+
+
