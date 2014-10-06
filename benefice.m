39abe84075e264736049d72a9d42f7e92b3a16fb
JeanMarchal <jmarchal@if501-213-06.insa-lyon.fr>
2014-10-06 14:31:12 +0200

    Nouvelle fonction bénéfice + ex graphes.

--- benefice.m
+++ benefice.m
@@ -1,16 +1,12 @@
-function [ x, fval, ConstraintsA, ConstraintsB ] = benefice(T1,T2,T41,T42,T5,ConstraintsA,ConstraintsB)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [ best_x, f_ben ] = benefice()
+%BENFICE Calcule le bénéfice maximal avec les contraintes de bases
+%   Renvoie la meilleure combinaison de produits (best_x) et la méthode de
+%   calcul du bénéfice (f_ben). Donc le bénéfice maximal est f_ben * best_x
+
+    matrix();
     T1T = T1';
-    fmax = T41 - (T42 * T2 + T5 * T1T);
-    f = fmax .* -1;
-    [x,fval] = linprog(f,ConstraintsA,ConstraintsB,[],[],zeros(6,1));
-    fval = fval.*-1;
-    
-    %On ajoute à la matrice B des contraintes la valeur de 80% de bénéfice
-    %qu'on veut
-    ConstraintsB = [ConstraintsB;(fval*0.8)];
-    %On ajoute à la matrice A les valeurs correspondantes de produits pour
-    %les 80% qu'on veut. A terminer...
-    ConstraintsA = [ConstraintsA; (x.*0.8)'];
+    f_ben = T41 - (T42 * T2 + T5 * T1T);
+
+    % Minimiser -f_ben <=> maximiser f_ben
+    best_x = linprog(-f_ben,ConstraintsA,ConstraintsB,[],[],zeros(6,1));
 end
---
+++ graphes_commercial.m
@@ -0,0 +1,26 @@
+function graphes_commercial ()
+    matrix();
+    [best_x_ben, f_ben] = benefice();
+    ben_max = f_ben * best_x_ben;
+
+    ConstraintsA(11, :) = -f_ben;
+
+    famille_1 = zeros(101, 1);
+    famille_2 = zeros(101, 1);
+    for i = 0:100
+        pourcentage = i / 100;
+
+        f_difference = [1 1 1 -1 -1 -1]';
+        f_famille_1 = [1 1 1 0 0 0];
+        f_famille_2 = [0 0 0 1 1 1];
+
+        ConstraintsB(11) = -ben_max * pourcentage;
+        best_x_commercial = linprog(f_difference, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
+
+        % L'indexation commence à 1, donc de 1 à 101 au lieu de 0 à 100
+        famille_1(i + 1) = f_famille_1 * best_x_commercial;
+        famille_2(i + 1) = f_famille_2 * best_x_commercial;
+    end
+    
+    plot(0:0.01:1, famille_1, '-', 0:0.01:1, famille_2, 'ro');
+end
