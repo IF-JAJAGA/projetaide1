ae78f94c465c14c872ba70ed7535f32544294117
albertorevel <alberto.revel@gmail.com>
2014-10-07 09:30:52 +0200

    Calcul des matrices en dehors du 'for'

--- graphes_cgt.m
+++ graphes_cgt.m
@@ -11,24 +11,21 @@ function [X_3, f_cgt_3,f_cgt_5 f_cgt_35] = graphes_cgt( )
     cgt_res_3 = zeros(101, 1);
     cgt_res_5 = zeros(101, 1);
     cgt_res_35 = zeros(101, 1);
-    for i = 0:100
-        pourcentage = i / 100;
-        
-        Tps = sum(T1,2);
+    
+	Tps = sum(T1,2);
         T1l_3 = [T1(:,3)];
         T1l_5 = [T1(:,5)];
         T1l_35 = [T1(:,3) T1(:,5)];
         Tpsl_3 = sum(T1l_3,2);
         Tpsl_5 = sum(T1l_5,2);
         Tpsl_35 = sum(T1l_35,2);
-
-%         f_cgt_3 = (Tpsl_3 ./ Tps);
-%         f_cgt_5 = (Tpsl_5 ./ Tps);
-%         f_cgt_35 = (Tpsl_35 ./ Tps);
         
         f_cgt_3 = Tpsl_3;
         f_cgt_5 = Tpsl_5;
         f_cgt_35 = Tpsl_35;
+	
+	for i = 0:100
+        pourcentage = i / 100;
 
         ConstraintsB(11) = -ben_max * pourcentage;
         best_x_cgt_3 = linprog(f_cgt_3, ConstraintsA, ConstraintsB, [], [], zeros(6, 1), []);
@@ -47,9 +44,7 @@ function [X_3, f_cgt_3,f_cgt_5 f_cgt_35] = graphes_cgt( )
     best_x_cgt_3 = linprog(f_cgt_3, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
     best_x_cgt_35 = linprog(f_cgt_35, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
     best_x_cgt_5 = linprog(f_cgt_5, ConstraintsA, ConstraintsB, [], [], zeros(6, 1));
-    
-    X_5 = best_x_cgt_5
-    X_3 = best_x_cgt_3
+
     X_35 = best_x_cgt_35
     
 end
