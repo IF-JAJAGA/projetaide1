b18271b80f5d4dda7ed997bd9d0a90754294cd90
unknown <gmonod@IF501-204-06.insa-lyon.fr>
2014-10-09 18:41:00 +0200

    Added beginning of part 2.

--- benefice.m
+++ benefice.m
@@ -1,13 +1,17 @@
-function [ best_x, f_ben ] = benefice()
+function [ best_x, f_ben ] = benefice( ben_max )
 %BENFICE Calcule le b�n�fice maximal avec les contraintes de bases
 %   Renvoie la meilleure combinaison de produits (best_x) et la m�thode de
 %   calcul du b�n�fice (f_ben). Donc le b�n�fice maximal est f_ben * best_x
 
-    [ConstraintsA, ConstraintsB, T1, T2, T3, T41, T42, T5] = matrix();
-    %matrix();
+    matrix();
     T1T = T1';
     f_ben = T41 - (T42 * T2 + T5 * T1T);
 
+    if nargin > 0
+        ConstraintsA(11, :) = f_ben;
+        ConstraintsB(11) = ben_max;
+    end
+    
     % Minimiser -f_ben <=> maximiser f_ben
     best_x = linprog(-f_ben',ConstraintsA,ConstraintsB,[],[],zeros(6,1), []);
 end
--- partie2.m
+++ partie2.m
@@ -1,9 +1,15 @@
+function [Gains, Optim] = partie2 ( VOptim, ben_max )
+
 matrix
 
 X = zeros(5,6);
 F = zeros(6,5);
 
-[X(1,:),F(:,1)] = benefice;
+if nargin < 2
+    [X(1,:),F(:,1)] = benefice;
+else
+    [X(1,:),F(:,1)] = benefice(ben_max);
+end
 [X(2,:),F(:,2)] = atelier;
 [X(3,:),F(:,3)] = stocks;
 [X(4,:),F(:,4)] = commercial;
@@ -13,6 +19,10 @@ clc
 
 Gains = X * F;
 Optim = Gains;
+
+if nargin < 1
+    VOptim = Optim;
+end
 
 for i = 1:2
     Gains(:, i) = Gains(:, i) ./ VOptim(i, i);
@@ -27,3 +37,5 @@ Gains(:, 4) = ones(5, 1) - (Optim(:, 4) ./ VOptim(2, 2));
 
 % Cas particulier de la cgt (moins d'utilisation machines 3_5 = mieux)
 Gains(:, 5) = VOptim(5, 5) ./ Gains(:, 5);
+
+end
